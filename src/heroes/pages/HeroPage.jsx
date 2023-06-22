62647d7759fa34922c5b01b6ed9b876594298d41
Kevincevas <cevas102@gmail.com>
2023-06-22 12:18:07 -0500

    update

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { Navigate, useParams } from 'react-router-dom'
+import { getHeroById } from '../helpers';
+
 
 export const HeroPage = () => {
+
+  const {id} = useParams();
+  const hero = getHeroById(id);
+  
+  // Hero don't exist
+  if (!hero) {
+    return <Navigate to="/marvel"/>;
+  }
+  
+
   return (
     <>
         <h1>HeroPage</h1>
